f7712076f26310e255c1643a2dce6decf88e88fc
Birkin James Diana <birkin.diana@gmail.com>
2024-02-01 07:50:10 -0500

    updates read-lines.

--- day01/puzzle_01_rust/src/main.rs
+++ day01/puzzle_01_rust/src/main.rs
@@ -1,32 +1,15 @@
-// use std::fs::File;
 use std::fs::read_to_string;
 
 fn main() {
-    // open file ----------------------------------------------------
+    //-- open file --------------------------------------------------
     // based on <https://doc.rust-lang.org/stable/rust-by-example/std_misc/file/read_lines.html>
-
-    // let lines: Vec<String> = read_to_string("../puzzle_01_source.txt").unwrap_or_else(|err| {
-    //     panic!("could not open file; error, ``{}``", err);
-    // }).lines().map(|s| s.to_string()).collect();
-
-    let mut lines = Vec::new();
-
-    // for line in read_to_string("../puzzle_01_source.txt").unwrap().lines() {
-    //     result.push(line.to_string())
-    // }
-
-    for line in read_to_string("../puzzle_01_source.txt")
-        .unwrap_or_else(|err| {
-            panic!("could not open file; error, ``{}``", err);
-        })
-        .lines()
-    {
-        lines.push(line.to_string())
+    let contents: String = read_to_string("../puzzle_01_source.txt").unwrap_or_else(|err| {
+        panic!("could not open file; error, ``{}``", err);
+    });
+    // let zz: () = contents;
+    //-- process lines ----------------------------------------------
+    for _line in contents.lines() {
+        // println!("{}", line);
+        let _foo = "bar";
     }
-
-    // let _opened_file = File::open("../puzzle_01_source.txt").unwrap_or_else(|err| {
-    //     panic!("could not open file; error, ``{}``", err);
-    // });
-
-    println!("Hello, world!");
 }
