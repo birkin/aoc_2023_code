b3034ecb170e961432afb270780692f4ce5aa86b
Birkin James Diana <birkin.diana@gmail.com>
2024-02-01 06:56:54 -0500

    starts open file.

--- day01/puzzle_01_rust/src/main.rs
+++ day01/puzzle_01_rust/src/main.rs
@@ -1,3 +1,9 @@
+use std::fs::File;
+
 fn main() {
+    // open file ----------------------------------------------------
+    let _opened_file = File::open("../puzzle_01_source.txt").unwrap_or_else(|err| {
+        panic!("could not open file; error, ``{}``", err);
+    });
     println!("Hello, world!");
 }
