23406844ee60e41d0eea410edba579c98ca16664
Birkin James Diana <birkin.diana@gmail.com>
2024-02-01 21:18:40 -0500

    tries out rayon.

--- day01/puzzle_01_rust/src/main.rs
+++ day01/puzzle_01_rust/src/main.rs
@@ -1,3 +1,4 @@
+use rayon::prelude::*;
 use std::fs::read_to_string;
 
 fn main() {
@@ -9,20 +10,33 @@ fn main() {
     // let zz: () = contents;  // my (working) hack attempt at python's `type(contents)` to inspect the type
     //-- process lines ----------------------------------------------
     let mut numbers: Vec<i32> = Vec::new();
-    for line in contents.lines() {
-        // println!("{}", line);
-        let _foo = "bar";
-        let first_number: i32 = get_first_number(&line);
-        let second_number: i32 = get_second_number(&line);
+
+    // Use a Mutex to safely access `numbers` across threads
+    let numbers_mutex = std::sync::Mutex::new(&mut numbers);
+
+    // for line in contents.lines() {
+    //     // println!("{}", line);
+    //     let _foo = "bar";
+    //     let first_number: i32 = get_first_number(&line);
+    //     let second_number: i32 = get_second_number(&line);
+    //     let new_number: i32 = (first_number * 10) + second_number;
+    //     numbers.push(new_number);
+    // }
+
+    let lines: Vec<&str> = contents.lines().collect();
+
+    lines.par_iter().for_each(|&line| {
+        let first_number: i32 = get_first_number(line);
+        let second_number: i32 = get_second_number(line);
         let new_number: i32 = (first_number * 10) + second_number;
-        numbers.push(new_number);
-    }
+
+        // Lock the mutex to push to the vector safely
+        numbers_mutex.lock().unwrap().push(new_number);
+    });
+
     // println!("numbers, ``{:?}``", numbers);
-    //-- sum numbers ------------------------------------------------
-    let mut sum: i32 = 0;
-    for number in numbers {
-        sum += number;
-    }
+    //-- sum numbers in parallel ------------------------------------
+    let sum: i32 = numbers.par_iter().sum();
     //-- print result -----------------------------------------------
     println!("sum_of_numbers: {}", sum);
 }
