90e1752514f27350ad501464d635738c2aff6547
Birkin James Diana <birkin.diana@gmail.com>
2024-02-25 02:47:58 -0500

    updates readme with nim results.

--- day01/puzzle_01.nim
+++ day01/puzzle_01.nim
@@ -13,7 +13,7 @@ import strformat
 proc get_first_number( txt: string ): int =
     #[  Iterates through the characters to get the first number. 
         Called by readLinesFromFile(). ]#
-    echo fmt"txt, ``{txt}``"
+    # echo fmt"txt, ``{txt}``"
     var num: int = 0
     for character in txt:
         # echo fmt"character, ``{character}``"
@@ -30,12 +30,12 @@ proc get_first_number( txt: string ): int =
 proc get_second_number( txt: string ): int =
     #[  Reverses the string and calls get_first_number(). 
         Called by `readLinesFromFile()`. ]#
-    echo fmt"txt initially, ``{txt}``"
+    # echo fmt"txt initially, ``{txt}``"
     var invertedTxt = newSeq[char](txt.len)
     for i in 0..<txt.len:
         invertedTxt[i] = txt[txt.len - i - 1]
     let invertedTxtStr = join(invertedTxt, "")  # Join characters without a delimiter
-    echo fmt"invertedTxtStr, ``{invertedTxtStr}``"
+    # echo fmt"invertedTxtStr, ``{invertedTxtStr}``"
     let num = get_first_number(invertedTxtStr)  # Assuming get_first_number is defined elsewhere
     return num
 
@@ -83,13 +83,13 @@ proc readLinesFromFile(filename: string): seq[int] =
     for line in file.lines:
         # Use the index as needed, similar to the i in Python's enumerate
         let first_number: int = get_first_number(line)
-        echo fmt"Line {index}, first_number: `{first_number}`"
+        # echo fmt"Line {index}, first_number: `{first_number}`"
         
         let second_number: int = get_second_number(line)
-        echo fmt"Line {index}, second_number: `{second_number}`"
+        # echo fmt"Line {index}, second_number: `{second_number}`"
 
         let num: int = parseInt( $first_number & $second_number )
-        echo fmt"Line {index}, num: `{num}`"
+        # echo fmt"Line {index}, num: `{num}`"
         
         numbers.add(num)
 
@@ -104,8 +104,8 @@ proc readLinesFromFile(filename: string): seq[int] =
 ## main code block --------------------------------------------------
 let filename = "./puzzle_01_source.txt"
 let numbers_sequence: seq[int] = readLinesFromFile(filename)
-echo fmt"numbers_sequence: `{numbers_sequence}`"
-echo "typeof( numbers_sequence ), ", typeof( numbers_sequence )  # yields: typeof( numbers_sequence ), seq[int]
+# echo fmt"numbers_sequence: `{numbers_sequence}`"
+# echo "typeof( numbers_sequence ), ", typeof( numbers_sequence )  # yields: typeof( numbers_sequence ), seq[int]
 
 import sequtils
 
