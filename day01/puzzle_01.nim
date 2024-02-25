f3d42a2d1def7b183d8bd8c9d9caacbcbffa7316
Birkin James Diana <birkin.diana@gmail.com>
2024-02-25 01:55:34 -0500

    grabs first and second number.

--- day01/puzzle_01.nim
+++ day01/puzzle_01.nim
@@ -7,15 +7,48 @@
 import strutils
 import streams
 import strformat
+import sequtils
 
 
 proc get_first_number( txt: string ): int =
+    #[  Iterates through the characters to get the first number. 
+        Called by readLinesFromFile(). ]#
+    echo fmt"txt, ``{txt}``"
     var num: int = 0
     for character in txt:
+        # echo fmt"character, ``{character}``"
         if character.isDigit():
-            num = int( character )
+            # echo fmt"character is digit"
+            # echo "typeof( character ), ", typeof( character )
+            # num = int( character )  # doesn't work, returns an integer for the ASCII value
+            num = parseInt( $character )  # Use `parseInt` to convert a string to an int
+            # echo fmt"num, ``{num}``"
             break
     return num
+
+
+proc get_second_number( txt: string ): int =
+    #[  Reverses the string and calls get_first_number(). 
+        Called by `readLinesFromFile()`. ]#
+    echo fmt"txt initially, ``{txt}``"
+    var invertedTxt = newSeq[char](txt.len)
+    for i in 0..<txt.len:
+        invertedTxt[i] = txt[txt.len - i - 1]
+    let invertedTxtStr = join(invertedTxt, "")  # Join characters without a delimiter
+    echo fmt"invertedTxtStr, ``{invertedTxtStr}``"
+    let num = get_first_number(invertedTxtStr)  # Assuming get_first_number is defined elsewhere
+    return num
+
+
+# proc get_second_number( txt: string ): int =
+#     #[  Reverses the string and calls get_first_number(). 
+#         Called by `readLinesFromFile()`. ]#
+#   var invertedTxt = newSeq[char](txt.len)  # Use `var` to ensure mutability
+#   for i in 0..<txt.len:
+#     invertedTxt[i] = txt[txt.len - i - 1]
+#   let invertedTxtStr = string(invertedTxt)  # Direct conversion to string
+#   let num = get_first_number(invertedTxtStr)  # Assuming get_first_number is defined elsewhere
+#   return num
 
 
 proc readLinesFromFile(filename: string): seq[string] =
@@ -24,11 +57,34 @@ proc readLinesFromFile(filename: string): seq[string] =
     let file = open(filename, fmRead)
     defer: file.close()  # ensures file is closed when the proc exits
     ## process lines ------------------------------------------------
+    var index = 0  # Initialize the index
     for line in file.lines:
-        lines.add(line)
-        var first_number: int = get_first_number( line )
-        echo fmt"first_number, ``{first_number}``"
+        # Use the index as needed, similar to the i in Python's enumerate
+        var first_number: int = get_first_number(line)
+        echo fmt"Line {index}, first_number: `{first_number}`"
+        
+        var second_number: int = get_second_number(line)
+        echo fmt"Line {index}, second_number: `{second_number}`"
+
+        index += 1  # Manually increment the index
+        if index > 5:
+            break
+
     return lines
+
+# proc readLinesFromFile(filename: string): seq[string] =
+#     ## open the file ------------------------------------------------
+#     var lines: seq[string] = @[]
+#     let file = open(filename, fmRead)
+#     defer: file.close()  # ensures file is closed when the proc exits
+#     ## process lines ------------------------------------------------
+#     for line in file.lines:
+#         # lines.add(line)
+#         var first_number: int = get_first_number( line )
+#         echo fmt"first_number, ``{first_number}``"
+#         var second_number: int = get_second_number( line )
+#         echo fmt"second_number, ``{second_number}``"
+#     return lines
 
 
 ## main code block --------------------------------------------------
